ded6a4d0a81cef4264c91b3753f635dba73ce7f1
Zheng Tang <oscartang1120@gmail.com>
2019-09-17 18:34:28 -0700

    tokenizer

--- mapping.py
+++ mapping.py
@@ -25,6 +25,7 @@ if __name__ == '__main__':
         print ("1")
         model = torch.load(args.model)
         bert = model.bert
+        tokenizer = model.tokenizer
     else:
         print ("2")
         tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
