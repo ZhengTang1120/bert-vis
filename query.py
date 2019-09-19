4eb784272526db5689a074fe3da265273906a924
Zheng Tang <oscartang1120@gmail.com>
2019-09-19 12:28:01 -0700

    update view

--- query.py
+++ query.py
@@ -5,42 +5,62 @@ import json
 # from random import sample
 
 (sentences, n_words, word2id, points) = pickle.load( open( 'save.p', 'rb' ) )
+(sentences2, n_words2, word2id2, points2) = pickle.load( open( 'save2.p', 'rb' ) )
 
 def get_result(query):
     
     xs = []
     ys = []
+    xs2 = []
+    ys2 = []
     res = []
     for eid, sid, wid in word2id[query]:
         point =  points[eid]
+        point2 =  points2[eid]
         sentence = sentences[sid][:]
         sentence1 = sentence[:wid]
         sentence2 = sentence[wid+1:]
         query = sentence[wid]
         sentence1 = [w for w in sentence1 if w != '[CLS]' and w != '[SEP]']
         sentence2 = [w for w in sentence2 if w != '[CLS]' and w != '[SEP]']
-        jl = {'X': float(point[0]), 'Y': float(point[1]), 'Sentence1': ' '.join(sentence1), 'query': query, 'Sentence2': ' '.join(sentence2)}
+        jl = {'X': float(point[0]), 'Y': float(point[1]), 'X2': float(point2[0]), 'Y2': float(point2[1]), 'Sentence1': ' '.join(sentence1), 'query': query, 'Sentence2': ' '.join(sentence2)}
         xs.append(point[0])
         ys.append(point[1])
+        xs2.append(point2[0])
+        ys2.append(point2[1])
         res.append(jl)
     return {"scores": json.dumps(res), "x0": math.floor(min(xs)), "x1": math.ceil(max(xs)), 
-            "y0": math.floor(min(ys)), "y1": math.ceil(max(ys))}
+            "y0": math.floor(min(ys)), "y1": math.ceil(max(ys)),
+            "x02": math.floor(min(xs2)), "x12": math.ceil(max(xs2)), 
+            "y02": math.floor(min(ys2)), "y12": math.ceil(max(ys2))}
 
-# for word in word2id:
-#     res = get_result(word)
-#     with open("static/words/%s.js"%word, "w") as f:
-#         f.write("var scores = ")
-#         f.write(res["scores"])
-#         f.write(";\n")
-#         f.write("var x0 = ")
-#         f.write(str(res["x0"]))
-#         f.write(";\n")
-#         f.write("var x1 = ")
-#         f.write(str(res["x1"]))
-#         f.write(";\n")
-#         f.write("var y0 = ")
-#         f.write(str(res["y0"]))
-#         f.write(";\n")
-#         f.write("var y1 = ")
-#         f.write(str(res["y1"]))
-#         f.write(";\n")
+for word in word2id:
+    res = get_result(word)
+    with open("static/words/%s.js"%word, "w") as f:
+        f.write("var scores = ")
+        f.write(res["scores"])
+        f.write(";\n")
+        f.write("var x0 = ")
+        f.write(str(res["x0"]))
+        f.write(";\n")
+        f.write("var x1 = ")
+        f.write(str(res["x1"]))
+        f.write(";\n")
+        f.write("var y0 = ")
+        f.write(str(res["y0"]))
+        f.write(";\n")
+        f.write("var y1 = ")
+        f.write(str(res["y1"]))
+        f.write(";\n")
+        f.write("var x02 = ")
+        f.write(str(res["x02"]))
+        f.write(";\n")
+        f.write("var x12 = ")
+        f.write(str(res["x12"]))
+        f.write(";\n")
+        f.write("var y02 = ")
+        f.write(str(res["y02"]))
+        f.write(";\n")
+        f.write("var y12 = ")
+        f.write(str(res["y12"]))
+        f.write(";\n")
--- static/script.js
+++ static/script.js
@@ -1,11 +1,11 @@
-var x_scale = d3.scaleLinear().domain([x0, x1]).range([25, 800]);
-var y_scale = d3.scaleLinear().domain([y0, y1]).range([800, 25]);
+var x_scale = d3.scaleLinear().domain([x0, x1]).range([25, 475]);
+var y_scale = d3.scaleLinear().domain([y0, y1]).range([475, 25]);
 
 div = d3.select("#main_div").append("div");
-var s1 = div.append("svg").attr("id", "scatterplot_1").attr("height", 825).attr("width", 825);
+
+var s1 = div.append("svg").attr("id", "scatterplot_1").attr("height", 500).attr("width", 500);
 var enter_s1 = s1.append("g").attr("class", "node").selectAll("circle").data(scores).enter().append("circle");
 enter_s1.attr("cx", function(d) {
-    console.log(d.X);
     return x_scale(d.X);
 });
 enter_s1.attr("cy", function(d) {
@@ -15,8 +15,24 @@ enter_s1.attr("r", 4);
 enter_s1.attr("fill", "grey");
 
 var g_y = s1.append("g").attr("transform", "translate(25,0)").call(d3.axisLeft(y_scale).ticks(5));
-var g_x = s1.append("g").attr("transform", "translate(0,800)").call(d3.axisBottom(x_scale).ticks(5));
+var g_x = s1.append("g").attr("transform", "translate(0,475)").call(d3.axisBottom(x_scale).ticks(5));
 
+var x_scale_2 = d3.scaleLinear().domain([x02, x12]).range([25, 475]);
+var y_scale_2 = d3.scaleLinear().domain([y02, y12]).range([475, 25]);
+
+var s2 = div.append("svg").attr("id", "scatterplot_2").attr("height", 500).attr("width", 500);
+var enter_s2 = s2.append("g").attr("class", "node").selectAll("circle").data(scores).enter().append("circle");
+enter_s2.attr("cx", function(d) {
+    return x_scale_2(d.X2);
+});
+enter_s2.attr("cy", function(d) {
+    return y_scale_2(d.Y2);
+});
+enter_s2.attr("r", 4);
+enter_s2.attr("fill", "grey");
+
+var g_y = s2.append("g").attr("transform", "translate(25,0)").call(d3.axisLeft(y_scale_2).ticks(5));
+var g_x = s2.append("g").attr("transform", "translate(0,475)").call(d3.axisBottom(x_scale_2).ticks(9));
 
 var previous_b = null;
 var brush_1 = s1.append("g")
@@ -37,6 +53,10 @@ function incircle(p, c) {
 function click(d) {
     d = d3.mouse(this);
     enter_s1.classed("selected", d && function(e) {
+        c = [x_scale(e.X), y_scale(e.Y)];
+        return incircle(d, c);
+    });
+    enter_s2.classed("selected", d && function(e) {
         c = [x_scale(e.X), y_scale(e.Y)];
         return incircle(d, c);
     });
@@ -65,8 +85,57 @@ function start() {
 function brushed() {
     var selection = d3.event.selection;
     enter_s1.classed("selected", selection && function(d) {
-        return selection[0][0] <= x_scale(d.SATM) && x_scale(d.SATM) <= selection[1][0] &&
-            selection[0][1] <= y_scale(d.SATV) && y_scale(d.SATV) <= selection[1][1];
+        return selection[0][0] <= x_scale(d.X) && x_scale(d.X) <= selection[1][0] &&
+            selection[0][1] <= y_scale(d.Y) && y_scale(d.Y) <= selection[1][1];
+    });
+    enter_s2.classed("selected", selection && function(d) {
+        return selection[0][0] <= x_scale(d.X) && x_scale(d.X) <= selection[1][0] &&
+            selection[0][1] <= y_scale(d.Y) && y_scale(d.Y) <= selection[1][1];
+    });
+    d3.select("#values").remove();
+}
+
+var brush_2 = s2.append("g")
+    .attr("class", "brush")
+    .call(d3.brush()
+        .on("start", start)
+        .on("brush end", brushed_2))
+    .on("click", click_2);
+
+function click_2(d) {
+    d = d3.mouse(this);
+    enter_s1.classed("selected", d && function(e) {
+        c = [x_scale_2(e.X2), y_scale_2(e.Y2)];
+        return incircle(d, c);
+    });
+    enter_s2.classed("selected", d && function(e) {
+        c = [x_scale_2(e.X2), y_scale_2(e.Y2)];
+        return incircle(d, c);
+    });
+    var sel = s2.selectAll(".selected").data();
+    d3.select("#values").remove();
+    var tb = d3.select("#main_div").append("div").attr("id", "values").selectAll("table").data(sel).enter().append("table")
+    var td = tb.append("tr").append("td")
+    td.append("span").text(function(d) {
+        return d.Sentence1;
+    })
+    td.append("span").attr("style","color:red").text(function(d) {
+        return " "+d.query+" ";
+    })
+    td.append("span").text(function(d) {
+        return d.Sentence2;
+    })
+}
+
+function brushed_2() {
+    var selection = d3.event.selection;
+    enter_s2.classed("selected", selection && function(d) {
+        return selection[0][0] <= x_scale_2(d.X2) && x_scale_2(d.X2) <= selection[1][0] &&
+            selection[0][1] <= y_scale_2(d.Y2) && y_scale_2(d.Y2) <= selection[1][1];
+    });
+    enter_s1.classed("selected", selection && function(d) {
+        return selection[0][0] <= x_scale_2(d.X2) && x_scale_2(d.X2) <= selection[1][0] &&
+            selection[0][1] <= y_scale_2(d.Y2) && y_scale_2(d.Y2) <= selection[1][1];
     });
     d3.select("#values").remove();
 }
