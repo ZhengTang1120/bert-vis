0b6662c2e6dac055fa082cbbfbc9cd6747d21803
Zheng Tang <oscartang1120@gmail.com>
2019-09-19 12:35:48 -0700

    Update mapping.py

--- mapping.py
+++ mapping.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     bert.to(device)
     bert.eval()
 
-    for i, (premise, hypothesis, cls) in enumerate(sample(train_set, 10000)):
+    for i, (premise, hypothesis, cls) in enumerate(dev_set):
         premise = '[CLS] '+ premise + ' [SEP]'
         hypothesis = hypothesis + ' [SEP]'
 
